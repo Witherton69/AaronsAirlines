14a470b3eae7ed2f5442d5ef5a314ea84e05007b
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-01-11 08:05:34 -0600

    Initial Commit

---
+++ AaronsAirlines/AaronsAirlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AaronsAirlinesApp.swift
+//  AaronsAirlines
+//
+//  Created by Katelyn Gosnell on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AaronsAirlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
